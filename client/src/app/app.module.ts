c20f9158ebe66f44eaab8ef3705006f6c294bb45
David Kielty <davidkielty@live.co.uk>
2023-10-12 09:33:46 +0100

    first commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,16 +7,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
